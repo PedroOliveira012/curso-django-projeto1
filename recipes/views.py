c4039d271a9b5ce6323548497dd4a660719a9ce7
Pedro Oliveira <pedro120403@gmail.com>
2023-08-28 09:48:50 -0300

    Atualizações sobre os Models
    - Habilitar o cadastro de receitas sem uma imagem cover
    - Criação do template para categorias
    - Inclusão de if's no template de home
    - Criação da url de categorias das receitas
    - Criação da view de receitas

--- recipes/views.py
+++ recipes/views.py
@@ -7,8 +7,20 @@ from .utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.all()
+    recipes = Recipe.objects.filter(
+        is_published=True
+    )
     return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    )
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes
     })
 
